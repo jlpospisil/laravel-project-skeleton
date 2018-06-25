da0cb9abffb3c42f974981f0a786bed8fcfb51c6
Josh Pospisil <josh.pospisil@gmail.com>
2018-06-25 17:03:28 -0500

    Changed orders to documents

--- database/migrations/2018_06_25_212713_create_documents_table.php
+++ database/migrations/2018_06_25_212713_create_documents_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrdersTable extends Migration
+class CreateDocumentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('documents', function (Blueprint $table) {
             $table->increments('id');
             $table->jsonb('data')->nullable();
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('documents');
     }
 }

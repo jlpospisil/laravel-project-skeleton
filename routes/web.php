4944efa688472af9800a12427787125a8c178410
Josh Pospisil <josh.pospisil@gmail.com>
2018-06-26 09:12:23 -0500

    Added route, controller, and view to manage passport tokens

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ Route::any('/logout', 'Auth\LoginController@logout');
 Route::get('/register', $redirect_to_home)->name('register');
 Route::any('/register', $redirect_to_home);
 
+// Passport routes
+Route::get('/oauth/settings', 'PassportController@index')->name('passport-settings');
+
 //
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', $redirect_to_home);

31282879a079074e606e27e5ce2b434b78ee1be7
Josh Pospisil <josh.pospisil@gmail.com>
2018-06-27 11:45:28 -0500

    Started implementing front end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,66 +8,21 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="app">
-    <!-- Navigation -->
-    <nav id="app-nav" class="navbar navbar-expand-sm fixed-top navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand mr-auto" href="/">
-                <strong>{{ config('app.name', '') }}</strong>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto"></ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav">
-                    @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('passport-settings') }}">
-                                <i class="fa fa-fw fa-key"></i>
-                                API Tokens
-                            </a>
-
-                            <a class="dropdown-item" href="/logout">
-                                <i class="fa fa-fw fa-sign-out"></i>
-                                Logout
-                            </a>
-                        </div>
-                    </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-
     <main class="py-3">
         @yield('content')
     </main>
 </div>
 </body>
+<script src="{{ mix ('js/app.js') }}"></script>
 </html>
